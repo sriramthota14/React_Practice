76f9529d78c9d2e060527850a95ee804bf988c33
sri1718 <sriram.thota14@gmail.com>
2025-03-08 23:13:27 -0500

    added css styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import React from "react";
 function App() {
   return (
     <div>
-      <h1>Welcome To React Interview Practice Session...</h1>
+      <h1
+        style={{
+          textAlign: "center",
+          color: "blue",
+          marginTop: "10px",
+          backgroundColor: "lightgray",
+          padding: "20px",
+        }}
+      >
+        Welcome To React Interview Practice Session...
+      </h1>
     </div>
   );
 }
